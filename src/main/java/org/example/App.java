596081454b89a07946c4b9c8ad1ab0a37d26e934
Bukovinszki László <bukovinszkilaszlo@gmail.com>
2025-10-03 11:20:55 +0200

    two pieces

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,8 +6,10 @@ package org.example;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        System.out.println(a()+b());
     }
+
+    private static String a() {return "Hello, ";}
+    private static String b() {return "World! ";}
 }
